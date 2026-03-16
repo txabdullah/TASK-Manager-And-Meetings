ebbfca8875f3b0600143dabf8c13bb41fd88aef1
Muhammad Abdullah <abdullahgurmani555@gmail.com>
2026-03-16 20:54:14 +0500

    feat: Part 4 - Team workspace system (CRUD, invite, members)

--- backend/apps/teams/admin.py
+++ backend/apps/teams/admin.py
@@ -1,1 +1,17 @@
 from django.contrib import admin
+from .models import Team, TeamMember, TeamInvite
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_by", "created_at")
+
+
+@admin.register(TeamMember)
+class TeamMemberAdmin(admin.ModelAdmin):
+    list_display = ("team", "user", "role", "joined_at")
+
+
+@admin.register(TeamInvite)
+class TeamInviteAdmin(admin.ModelAdmin):
+    list_display = ("team", "email", "role", "invited_by", "created_at")
